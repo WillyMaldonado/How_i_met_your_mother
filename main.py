0b070e04294fd4f7ea9aed1cf993b6ac27278b48
WillyMaldonado <124693730+WillyMaldonado@users.noreply.github.com>
2023-02-14 09:54:43 -0600

    main.py update y gitignore
    ignorar la carpeta modulos

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Guillermo Alejandro Maldonado Moreno\nNo.carne = 1503623')
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
